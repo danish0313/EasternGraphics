5708ceb22054df61c36db8ca448a0fcc590f720c
Danish Khan <dkhan@de.egr.lan>
2019-10-28 12:09:59 +0100

    styles added for each component seperatly

---
+++ src/components/filters/options/level/level.tsx
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import classes from "./level.module.css";
+import _ from "lodash";
+
+type MyProps = {
+  uniquelevels: string[],
+  levelHandler: any,
+  disablinglevel: any,
+  facilityvalue: string
+};
+
+export default class facility extends Component<MyProps> {
+  render() {
+    return (
+      <>
+        {this.props.facilityvalue.length ? (
+          <>
+            <label className={classes.label}>search by facility</label>
+
+            <select
+              className={classes.all}
+              onChange={e => this.props.levelHandler(e)}
+            >
+              <option value=""> </option>
+              {this.props.uniquelevels.map(
+                (level: string, i: number) => {
+                  return (
+                    <option
+                      key={level + i}
+                      disabled={
+                        _.indexOf(this.props.uniquelevels, level) !==
+                        this.props.disablinglevel()
+                      }
+                      selected={
+                        _.indexOf(this.props.uniquelevels, level) ===
+                        this.props.disablinglevel()
+                      }
+                      value={level}
+                    >
+                      {" "}
+                      {level}
+                    </option>
+                  );
+                }
+              )}
+            </select>
+          </>
+        ) : (
+          <>
+            <label className={classes.label}>search by Level</label>
+
+            <select
+              className={classes.all}
+              onChange={e => this.props.levelHandler(e)}
+            >
+              <option value=""> </option>
+              {this.props.uniquelevels.map(
+                (level: string, i: number) => {
+                  return (
+                    <option key={level + i} value={level}>
+                      {" "}
+                      {level}
+                    </option>
+                  );
+                }
+              )}
+            </select>
+          </>
+        )}
+      </>
+    );
+  }
+}
