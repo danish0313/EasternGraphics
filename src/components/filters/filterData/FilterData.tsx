4ff00adca13545fe921aeaaab28d7472f3b62c08
Danish Khan <dkhan@de.egr.lan>
2019-10-28 12:07:47 +0100

    removed unnecessary state

--- src/components/filters/filterData/FilterData.tsx
+++ src/components/filters/filterData/FilterData.tsx
@@ -2,13 +2,16 @@
  import classes from './FilterData.module.css';
 
  type MyProps = {
-    filterdata: string[],
+  FacilityLevelFilter: any,
+  SearchFilter : any ,
+  SearchValue : string
  }
  export default class FilterData extends Component<MyProps> {
      render() {
-         return (
-             <div>
-                 {this.props.filterdata.map((data: any, index: number) => (
+ let filteredData = null;
+if(this.props.SearchValue.length > 0) {
+
+       filteredData = (this.props.SearchFilter().map((data: any, index: number) => (
         <div key={data.message}>
           <span> Facility: {data.facility} </span> <br /> <br />
           <span> Level: {data.level} </span> <br />
@@ -20,7 +23,27 @@
           <hr />
         </div>
              
-         ) )   }  
+         )))}else{
+
+          filteredData = (this.props.FacilityLevelFilter().map((data: any, index: number) => (
+            <div key={data.message}>
+              <span> Facility: {data.facility} </span> <br /> <br />
+              <span> Level: {data.level} </span> <br />
+              <span>message:</span> <br />
+              {data.message.split("\n").map((item: any, i: number) => {
+                return <p key={item + i}>{item} </p>;
+              })}
+              <p className={classes.timestamp}> TimeStamp: {new Date(data.timeStamp).toUTCString()} </p>
+              <hr />
+            </div>
+                 
+             )))}
+
+
+
+         return (
+             <div>
+              {filteredData}
          </div> )
      }
  }
