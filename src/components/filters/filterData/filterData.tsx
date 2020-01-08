148f1599080c6e295b5f2bf60bdd80ffa2fa8cb6
Danish Khan <dkhan@de.egr.lan>
2020-01-08 16:05:48 +0100

    added a comments dialog btn and dialog.

--- src/components/filters/filterData/filterData.tsx
+++ src/components/filters/filterData/filterData.tsx
@@ -3,7 +3,8 @@ import { Values } from '../../../App';
 import { DetailsList, DetailsListLayoutMode, SelectionMode } from 'office-ui-fabric-react/lib/DetailsList';
 import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
 import { Spinner, SpinnerSize } from 'office-ui-fabric-react/lib/Spinner';
-import { ActionButton, IIconProps } from 'office-ui-fabric-react';
+import { IconButton } from 'office-ui-fabric-react/lib/Button';
+import DialogLargeHeader from '../options/dialog/dialog';
 
 interface MyFilterDataProps {
     filteredArray: Array<Values>;
@@ -25,9 +26,23 @@ export interface Data {
     Content?: React.ReactNode;
     TimeStamp?: string | number;
     Copy?: React.ReactNode;
+    Dialog?: React.ReactNode;
 }
-const addFriendIcon: IIconProps = { iconName: 'Copy' };
-export default class FilterData extends Component<MyFilterDataProps> {
+
+interface MyFilterDataState {
+    dialogOpen: boolean;
+}
+
+export default class FilterData extends Component<MyFilterDataProps, MyFilterDataState> {
+
+    constructor(props: MyFilterDataProps) {
+        super(props);
+        this.state = {
+            dialogOpen: true,
+
+        };
+    }
+
     public render(): JSX.Element {
         // Populate with items for datalist.
         const results: Array<Values> = this.props.filteredArray.length > 0 ? this.props.filteredArray : this.props.arrayWithoutFilter;
@@ -38,49 +53,62 @@ export default class FilterData extends Component<MyFilterDataProps> {
                 Facility: i.facility,
                 Content: (
                     <pre>
-                       {i.content}
+                        {i.content}
                     </pre>
                 ),
                 TimeStamp: new Date(Number(i.date) * 1000).toLocaleString('en-US').split('/').join('-'),
                 Copy: (
-                    <ActionButton
+                    <IconButton
                         key={i.content}
-                        iconProps={addFriendIcon}
+                        iconProps={{ iconName: 'Copy' }}
+                        title="Copy"
+                        ariaLabel="Copy"
                         onClick={() => this.handleClick(i.content)}
+                        style={{ marginRight: '30px' }}
+                    />),
+                Dialog: (
+                    <IconButton
+                        onClick={this.showDialog}
+                        iconProps={{ iconName: 'Comment' }}
+                        title="comments"
+                        ariaLabel="Comments"
                         style={{ marginRight: '30px' }}
                     />)
             });
         }
 
         const columns: Array<DetailsListBasicItem> = [
-            { key: 'column1', name: '', fieldName: 'Copy', minWidth: 50, maxWidth: 100, isResizable: true },
-            { key: 'column2', name: 'Level', fieldName: 'Level', minWidth: 50, maxWidth: 100, isResizable: true },
-            { key: 'column3', name: 'Facility', fieldName: 'Facility', minWidth: 50, maxWidth: 100, isResizable: true },
-            { key: 'column4', name: 'Content', fieldName: 'Content', minWidth: 50, maxWidth: 1300, isResizable: true },
-            { key: 'column5', name: 'TimeStamp', fieldName: 'TimeStamp', minWidth: 100, maxWidth: 100, isResizable: true }
+            { key: 'column1', name: '', fieldName: 'Copy', minWidth: 50, maxWidth: 50, isResizable: true },
+            { key: 'column2', name: '', fieldName: 'Dialog', minWidth: 50, maxWidth: 50, isResizable: true },
+            { key: 'column3', name: 'Level', fieldName: 'Level', minWidth: 50, maxWidth: 100, isResizable: true },
+            { key: 'column4', name: 'Facility', fieldName: 'Facility', minWidth: 50, maxWidth: 100, isResizable: true },
+            { key: 'column5', name: 'Content', fieldName: 'Content', minWidth: 50, maxWidth: 1300, isResizable: true },
+            { key: 'column6', name: 'TimeStamp', fieldName: 'TimeStamp', minWidth: 100, maxWidth: 100, isResizable: true }
         ];
 
         return (
-            <div>{this.props.loading  ? (<Spinner label={this.props.label} ariaLive="assertive" labelPosition="bottom" size={SpinnerSize.large} />
-            ) :
-                (
-                    <Fabric>
-                        <DetailsList
-                            items={data}
-                            columns={columns}
-                            selectionMode={SelectionMode.none}
-                            setKey="set"
-                            layoutMode={DetailsListLayoutMode.justified}
-                            selectionPreservedOnEmptyClick={true}
-                            ariaLabelForSelectionColumn="Toggle selection"
-                            ariaLabelForSelectAllCheckbox="Toggle selection for all items"
-                            checkButtonAriaLabel="Row checkbox"
-                        />
-                    </Fabric>
-                )}
-            </div>);
-    }
+            <>
+                <DialogLargeHeader dialog={this.state.dialogOpen} dialogOpen={this.showDialog} DialogHide={this.closeDialog} />
 
+                <div>{this.props.loading ? (<Spinner label={this.props.label} ariaLive="assertive" labelPosition="bottom" size={SpinnerSize.large} />
+                ) :
+                    (
+                        <Fabric>
+                            <DetailsList
+                                items={data}
+                                columns={columns}
+                                selectionMode={SelectionMode.none}
+                                setKey="set"
+                                layoutMode={DetailsListLayoutMode.justified}
+                                selectionPreservedOnEmptyClick={true}
+                                ariaLabelForSelectionColumn="Toggle selection"
+                                ariaLabelForSelectAllCheckbox="Toggle selection for all items"
+                                checkButtonAriaLabel="Row checkbox"
+                            />
+                        </Fabric>
+                    )}
+                </div>   </>);
+    }
     private handleClick = (event: string) => {
         const textArea: HTMLTextAreaElement = document.createElement('textarea');
         textArea.value = event;
@@ -88,5 +116,12 @@ export default class FilterData extends Component<MyFilterDataProps> {
         textArea.select();
         document.execCommand('Copy');
         textArea.remove();
+    };
+
+    private showDialog = (): void => {
+        this.setState({ dialogOpen: false });
+    };
+    private closeDialog = (): void => {
+        this.setState({ dialogOpen: true });
     };
 }
---
+++ src/components/filters/options/dialog/dialog.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { Dialog, DialogType, DialogFooter } from 'office-ui-fabric-react/lib/Dialog';
+import { PrimaryButton, DefaultButton } from 'office-ui-fabric-react/lib/Button';
+import { Checkbox } from 'office-ui-fabric-react/lib/Checkbox';
+import { Stack } from 'office-ui-fabric-react/lib/Stack';
+import { TextField } from 'office-ui-fabric-react/lib/TextField';
+
+export interface DialogLargeHeaderProps {
+    dialog: boolean;
+    dialogOpen: () => void;
+    DialogHide: () => void;
+}
+// Used to add spacing between example checkboxes
+const stackTokens: object = { childrenGap: 10 };
+export default class DialogLargeHeader extends React.PureComponent<DialogLargeHeaderProps> {
+
+    public render(): JSX.Element {
+        return (
+            <div>
+                <Dialog
+                    hidden={this.props.dialog}
+                    onDismiss={this.props.DialogHide}
+                    dialogContentProps={{
+                        type: DialogType.largeHeader,
+                        title: 'Add Comments to ErrosLog',
+                        subText: 'Please enter your Comments for Specific ErrorLogs :)'
+                    }}
+                    modalProps={{
+                        isBlocking: false,
+                        styles: { main: { maxWidth: 450 } }
+                    }}
+                >
+                    <Stack tokens={stackTokens}>
+                        <Checkbox label="important" onChange={this.handleChange} />
+                        <Checkbox label="not important" onChange={this.handleChange} />
+                    </Stack>
+
+                    <TextField styles={{ root: { marginTop: '30px' } }} label="Comments TextArea..."  multiline={true} rows={3} />
+                    <DialogFooter>
+                        <PrimaryButton onClick={this.props.DialogHide} text="Save" />
+                        <DefaultButton onClick={this.props.DialogHide} text="Cancel" />
+                    </DialogFooter>
+                </Dialog>
+            </div>
+        );
+    }
+    private handleChange = (ev?: React.FormEvent<HTMLElement>, isChecked?: boolean) => {
+          const value: boolean | undefined = isChecked;
+    };
+
+}
