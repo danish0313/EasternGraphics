3ce401fe56f7d1a96648816978077c24bf9cb17a
Danish Khan <dkhan@de.egr.lan>
2020-01-08 13:23:23 +0100

    clear btn in side the searchbar now works. #28 resolved.

--- src/components/filters/options/searchBar/searchBar.tsx
+++ src/components/filters/options/searchBar/searchBar.tsx
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import { Label } from 'office-ui-fabric-react/lib/Label';
 import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
 interface MySearchBarProps {
-    searchHandler: (option: string , key: string  , value?: string , start?: number, end?: number ) => void;
+    searchHandler: (option: string, key: string, value?: string, start?: number, end?: number) => void;
     label: string;
 }
 interface MySearchBarState {
@@ -19,25 +19,24 @@ export default class SearchBar extends PureComponent<MySearchBarProps, MySearchB
     public render(): JSX.Element {
         return (
             <>
-                <Label style={{ textAlign: 'center' , marginTop: '10px' }}>
+                <Label style={{ textAlign: 'center', marginTop: '10px' }}>
                     {this.props.label}</Label>
                 <SearchBox
                     styles={{ root: { width: '60vh', margin: '0 auto', height: 40 } }}
                     placeholder="Search Your Content!"
                     iconProps={{ iconName: 'Filter' }}
                     onChange={this.handleClick}
+                    onClear={this.handleClick}
                 />
-
             </>
         );
     }
-
     private handleClick = (e: React.ChangeEvent<HTMLInputElement> | undefined, newValue?: string | undefined): void => {
         if (e == null) {
             return;
         }
         const value: string = e.target.value;
-        this.props.searchHandler('', '' , value);
+        this.props.searchHandler('', '', value);
     };
 
 }
