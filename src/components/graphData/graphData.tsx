963f8693c30dc077b7e6d912a67ee48c55b2a720
Danish Khan <dkhan@de.egr.lan>
2020-01-20 12:44:09 +0100

    graph data is improved.

--- src/components/graphData/graphData.tsx
+++ src/components/graphData/graphData.tsx
@@ -56,8 +56,8 @@ class GraphDetails extends React.PureComponent<RouteComponentProps> {
                                 <YAxis />
                                 <Tooltip />
                                 <Legend />
-                                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                                <Line type="monotone" name="facility" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                                <Line type="monotone" name="level" dataKey="uv" stroke="#82ca9d" />
                             </LineChart>
                         </div>
                     </div>
