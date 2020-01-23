96bb18c3125465388a2aec44559dd6d2e1986630
Danish Khan <dkhan@de.egr.lan>
2020-01-23 14:47:33 +0100

    graph data improved showing only level data.

--- src/components/graphData/graphData.tsx
+++ src/components/graphData/graphData.tsx
@@ -3,9 +3,14 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'r
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 interface Data {
     name: string;
-    pv: number;
-    uv: number;
-    amt: number;
+    info: number;
+    warning: number;
+    debug: number;
+    notice: number;
+    error: number;
+    config: number;
+    fatal: number;
+    date: string;
     key?: string | number;
     text?: string | number;
 }
@@ -13,25 +18,32 @@ class GraphDetails extends React.PureComponent<RouteComponentProps> {
     public render(): JSX.Element {
         const data: Array<Data> = [
             {
-                name: 'Mon', pv: 4000, uv: 2400, amt: 2400, key: 'facility', text: 'GF::eai:eproduct',
+                name: 'Mon', date: '2012', info: 4000, debug: 2400, warning: 2400, error: 200, notice: 700,
+                config: 300, fatal: 100, key: 'level', text: 'INFO',
             },
             {
-                name: 'Tue', pv: 3000, uv: 1398, amt: 200, key: 'facility', text: 'GF::fapish',
+                name: 'Tue', date: '2012', info: 3000, debug: 1398, warning: 200, error: 500, notice: 300,
+                config: 500, fatal: 400, key: 'level', text: 'DEBUG',
             },
             {
-                name: 'Wed', pv: 2000, uv: 9800, amt: 7800, key: 'level', text: 'INFO',
+                name: 'Wed', date: '2012', info: 2000, debug: 9800, warning: 7800, error: 400, notice: 200,
+                config: 900, fatal: 100, key: 'level', text: 'WARNING',
             },
             {
-                name: 'Tur', pv: 2780, uv: 3908, amt: 8450, key: 'level', text: 'WARNING',
+                name: 'Tur', date: '2012', info: 2780, debug: 3908, warning: 8450, error: 700, notice: 600,
+                config: 100, fatal: 500, key: 'level', text: 'ERROR',
             },
             {
-                name: 'Fri', pv: 1890, uv: 4800, amt: 2900, key: 'level', text: 'DEBUG',
+                name: 'Fri', date: '2012', info: 1890, debug: 4800, warning: 2900, error: 400, notice: 900,
+                config: 200, fatal: 800, key: 'level', text: 'NOTICE',
             },
             {
-                name: 'Sat', pv: 2390, uv: 3800, amt: 1200, key: 'level', text: 'ERROR',
+                name: 'Sat', date: '2012', info: 2390, debug: 3800, warning: 1200, error: 800, notice: 300,
+                config: 800, fatal: 300, key: 'level', text: 'CONFIG',
             },
             {
-                name: 'Sun', pv: 3490, uv: 4300, amt: 3400, key: 'host', text: 'https://c-live2b.pcon.eu/',
+                name: 'Sun', date: '2012', info: 3490, debug: 4300, warning: 3400, error: 200, notice: 700,
+                config: 500, fatal: 100, key: 'level', text: 'FATAL',
             },
         ];
 
@@ -51,13 +63,19 @@ class GraphDetails extends React.PureComponent<RouteComponentProps> {
                                 }}
                                 onClick={this.handleClick}
                             >
-                                <CartesianGrid strokeDasharray="3 3" />
+                                <CartesianGrid strokeDasharray="5 5" />
                                 <XAxis dataKey="name" />
                                 <YAxis />
-                                <Tooltip />
+                                <Tooltip
+                                />
                                 <Legend />
-                                <Line type="monotone" name="facility" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                                <Line type="monotone" name="level" dataKey="uv" stroke="#82ca9d" />
+                                <Line type="monotone" name="Info" dataKey="info" stroke="#8884d8" activeDot={{ r: 4 }} />
+                                <Line type="monotone" name="Warning" dataKey="warning" stroke="#605E5C" activeDot={{ r: 4 }} />
+                                <Line type="monotone" name="Debug" dataKey="debug" stroke="grey" activeDot={{ r: 4 }} />
+                                <Line type="monotone" name="Error" dataKey="error" stroke="red" activeDot={{ r: 4 }} />
+                                <Line type="monotone" name="Notice" dataKey="notice" stroke="green" activeDot={{ r: 4 }} />
+                                <Line type="monotone" name="Config" dataKey="config" stroke="lightblue" activeDot={{ r: 4 }} />
+                                <Line type="monotone" name="Fatal" dataKey="fatal" stroke="pink" activeDot={{ r: 4 }} />
                             </LineChart>
                         </div>
                     </div>
@@ -65,6 +83,7 @@ class GraphDetails extends React.PureComponent<RouteComponentProps> {
             </div>
         );
     }
+
     private handleClick = (e: any | undefined) => {
         if (e === null) {
             return;
