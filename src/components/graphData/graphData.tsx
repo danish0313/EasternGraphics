f4df45516e87d9832dee78106dd2f1f7791420c6
Danish Khan <dkhan@de.egr.lan>
2020-01-29 16:46:57 +0100

    custom tooltip , custom label and underline for each active line is added to the tooltip.

--- src/components/graphData/graphData.tsx
+++ src/components/graphData/graphData.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, RechartsFunction } from 'recharts';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 interface Data {
     name: string;
@@ -11,41 +11,96 @@ interface Data {
     config: number;
     fatal: number;
     date: string;
-    key?: string | number;
-    text?: string | number;
+    key: string | number;
+    text: string | number;
+    stroke: string;
+    color?: string;
+    value?: number;
+
 }
+
+interface Label {
+    stroke?: string;
+    value?: number;
+    x?: string;
+    y?: string;
+}
+
+// for  css underline
+let color: string = '';
 class GraphDetails extends React.PureComponent<RouteComponentProps> {
+
     public render(): JSX.Element {
         const data: Array<Data> = [
             {
-                name: 'Mon', date: '2012', info: 4000, debug: 2400, warning: 2400, error: 200, notice: 700,
-                config: 300, fatal: 100, key: 'level', text: 'INFO',
+                name: 'Mon', date: '12-4-2019', info: 4000, debug: 2400, warning: 2400, error: 200, notice: 700,
+                config: 300, fatal: 100, key: 'level', text: 'info', stroke: '#0078D4'
             },
             {
-                name: 'Tue', date: '2012', info: 3000, debug: 1398, warning: 200, error: 500, notice: 300,
-                config: 500, fatal: 400, key: 'level', text: 'DEBUG',
+                name: 'Tue', date: '10-20-2019', info: 3000, debug: 1398, warning: 200, error: 500, notice: 400,
+                config: 500, fatal: 400, key: 'level', text: 'debug', stroke: 'green'
             },
             {
-                name: 'Wed', date: '2012', info: 2000, debug: 9800, warning: 7800, error: 400, notice: 200,
-                config: 900, fatal: 100, key: 'level', text: 'WARNING',
+                name: 'Wed', date: '10-21-2019', info: 2000, debug: 9800, warning: 7800, error: 400, notice: 100,
+                config: 900, fatal: 100, key: 'level', text: 'warning', stroke: 'brown'
             },
             {
-                name: 'Tur', date: '2012', info: 2780, debug: 3908, warning: 8450, error: 700, notice: 600,
-                config: 100, fatal: 500, key: 'level', text: 'ERROR',
+                name: 'Tur', date: '10-22-2019', info: 2780, debug: 3908, warning: 8450, error: 700, notice: 600,
+                config: 2000, fatal: 500, key: 'level', text: 'error', stroke: 'red'
             },
             {
-                name: 'Fri', date: '2012', info: 1890, debug: 4800, warning: 2900, error: 400, notice: 900,
-                config: 200, fatal: 800, key: 'level', text: 'NOTICE',
+                name: 'Fri', date: '10-26-2019', info: 1890, debug: 4800, warning: 7900, error: 400, notice: 1100,
+                config: 200, fatal: 600, key: 'level', text: 'notice', stroke: 'darkblue'
             },
             {
-                name: 'Sat', date: '2012', info: 2390, debug: 3800, warning: 1200, error: 800, notice: 300,
-                config: 800, fatal: 300, key: 'level', text: 'CONFIG',
+                name: 'Sat', date: '10-27-2019', info: 2390, debug: 3800, warning: 9200, error: 800, notice: 300,
+                config: 800, fatal: 1600, key: 'level', text: 'config', stroke: 'grey'
             },
             {
-                name: 'Sun', date: '2012', info: 3490, debug: 4300, warning: 3400, error: 200, notice: 700,
-                config: 500, fatal: 100, key: 'level', text: 'FATAL',
+                name: 'Sun', date: '10-28-2019', info: 3490, debug: 4300, warning: 400, error: 200, notice: 700,
+                config: 500, fatal: 400, key: 'level', text: 'fatal', stroke: 'black'
             },
         ];
+
+        // CUSTOM Label for Graph
+        class CustomizedLabel extends React.PureComponent<Label> {
+            public render(): JSX.Element {
+                const {
+                    x, y, stroke, value,
+                } = this.props;
+
+                return <text x={x} y={y} dy={-4} fill={stroke} fontSize={10} textAnchor="middle">{value}</text>;
+            }
+        }
+
+        // CUSTOM ToolTip for Graph
+        class CustomTooltip extends React.PureComponent<any> {
+            public render(): JSX.Element | null {
+
+                const { active } = this.props;
+                if (active) {
+                    const { payload, label } = this.props;
+
+                    return (
+                        <div style={{ background: 'linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.1)', width: '15vh', padding: '10px' }}>
+                            <div style={{ fontWeight: 'bold' }}>{payload[0].payload.date}</div>
+                            <div style={{ fontWeight: 'bold' }}>{label} </div>
+                            <hr />
+                            <div> {payload.map((x: Data) =>
+                                <p
+                                    key={x.name}
+                                    style={{ color: `${x.color}`, fontWeight: 'bold', borderBottom: x.color === color ? '3px double' : 'none' }}
+                                >
+                                    {x.name}:  {x.value}
+                                </p>)}
+                            </div>
+
+                        </div>
+                    );
+                }
+                return null;
+            }
+        }
 
         return (
             <div>
@@ -55,69 +110,34 @@ class GraphDetails extends React.PureComponent<RouteComponentProps> {
                     <div className="ms-Grid-row">
                         <div className="ms-Grid-col ms-u-sm12 ms-u-md12 ms-u-lg12">
                             <LineChart
-                                width={500}
-                                height={300}
+                                width={1000}
+                                height={800}
                                 data={data}
                                 margin={{
                                     top: 5, right: 30, left: 20, bottom: 5,
                                 }}
-                            onClick={this.handleClick}
                             >
                                 <CartesianGrid strokeDasharray="5 5" />
-                                <XAxis dataKey="name" />
+                                <XAxis dataKey="name" height={60} />
                                 <YAxis />
                                 <Tooltip
+                                    content={<CustomTooltip
+                                    />}
+
                                 />
                                 <Legend />
-                                <Line
+                                {data.map((lines: Data) => (<Line
+                                    key={lines.name}
                                     type="monotone"
-                                    name="Info"
-                                    dataKey="info"
-                                    stroke="#8884d8"
-                                    activeDot={{ r: 4, onClick: (x: string, y: string) => console.log(x, y) }}
-                                />
-                                <Line
-                                    type="monotone"
-                                    name="Warning"
-                                    dataKey="warning"
-                                    stroke="#605E5C"
-                                    activeDot={{ r: 4, onClick: (x: string, y: string) => console.log(x, y) }}
-                                />
-                                <Line
-                                    type="monotone"
-                                    name="Debug"
-                                    dataKey="debug"
-                                    stroke="grey"
-                                    activeDot={{ r: 4, onClick: (x: string, y: string) => console.log(x, y) }}
-                                />
-                                <Line
-                                    type="monotone"
-                                    name="Error"
-                                    dataKey="error"
-                                    stroke="red"
-                                    activeDot={{ r: 4, onClick: (x: string, y: string) => console.log(x, y) }}
-                                />
-                                <Line
-                                    type="monotone"
-                                    name="Notice"
-                                    dataKey="notice"
-                                    stroke="green"
-                                    activeDot={{ r: 4, onClick: (x: string, y: string) => console.log(x, y) }}
-                                />
-                                <Line
-                                    type="monotone"
-                                    name="Config"
-                                    dataKey="config"
-                                    stroke="lightblue"
-                                    activeDot={{ r: 4, onClick: (x: string, y: string) => console.log(x, y) }}
-                                />
-                                <Line
-                                    type="monotone"
-                                    name="Fatal"
-                                    dataKey="fatal"
-                                    stroke="pink"
-                                    activeDot={{ r: 4, onClick: (x: string, y: string) => console.log(x, y) }}
-                                />
+                                    name={lines.text}
+                                    dataKey={lines.text}
+                                    stroke={lines.stroke}
+                                    label={<CustomizedLabel />}
+                                    strokeWidth={2}
+                                    activeDot={{ onClick: this.handleClick }}
+                                    onMouseOver={this.onMouseOver}
+                                    dot={false}
+                                />))}
                             </LineChart>
                         </div>
                     </div>
@@ -126,16 +146,24 @@ class GraphDetails extends React.PureComponent<RouteComponentProps> {
         );
     }
 
-    private handleClick = (e: any | undefined) => {
-        if (e === null) {
+    private onMouseOver = (e: any) => {
+        color = e.stroke;
+
+    };
+    private handleClick = (x: string, y: any) => {
+
+        if (y === null) {
             return;
         }
-        const text: string = e.activePayload[0].payload.text.toString();
-        const key: string = e.activePayload[0].payload.key.toString();
+        const text: string = y.dataKey.toLocaleUpperCase();
+        const key: string = y.payload.key;
+        const date: number = y.payload.date;
+       // console.log(text, key, date);
         this.props.history.push({
             pathname: '/', state: {
                 key: key,
-                text: text
+                text: text,
+                date: date
             }
         });
     };
